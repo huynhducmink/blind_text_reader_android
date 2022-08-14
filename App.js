a31a14029b701c2a86d23f5f50deaf4b49eb9b21
huynhducmink <cnhhuynhmink@gmail.com>
2022-08-14 21:46:49 +0700

    init commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Alert } from 'react-native'
+import { RNCamera } from 'react-native-camera'
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          style={{ flex: 1, alignItems: 'center' }}
+          ref={ref => {
+            this.camera = ref
+          }}
+          capture
+          type={RNCamera.Constants.Type.back}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  }
+})
+
+export default App
---
+++ README.md
@@ -0,0 +1,5 @@
+# React Native application for Android using AI for detecting text from camera and change to voice
+## Author
+### Huỳnh Đức Minh 
+### Trần Đức Dũng
+ 
