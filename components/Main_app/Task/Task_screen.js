00a79794cc37ef1256ef89d2ef3147801f2ccb49
huynhducmink <cnhhuynhmink@gmail.com>
2022-08-30 01:17:57 +0700

    Main app

--- components/Main_app/Task/Task_screen.js
+++ components/Main_app/Task/Task_screen.js
@@ -1,22 +1,44 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, 
+  StyleSheet, 
+  Image, 
+  TextInput, 
+  TouchableWithoutFeedback, 
+  Keyboard,
+ } from "react-native";
 
 export default class Task_screen extends React.Component{
   constructor(props){
     super(props)
+    this.state={
+      search_text:""
+    }
+    this.set_search_text = this.set_search_text.bind()
+  }
+
+  set_search_text = ( input ) => {
+    this.setState({search_text:input})
   }
 
   render(){
     return(
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.background}>
         <View style={styles.topcontainer}>
-          <Image source={require("../assets/Task_screen/tick_2.png")} style={styles.taskbar_icon}/>
-          <Image source={require("../assets/Task_screen/cal.png")} style={styles.taskbar_icon}/>
           <Image source={require("../assets/Task_screen/menu.png")} style={styles.taskbar_icon}/>
           <Image source={require("../assets/Task_screen/note.png")} style={styles.taskbar_icon}/>
+          <Image source={require("../assets/Task_screen/tick_2.png")} style={styles.taskbar_icon}/>
+          <Image source={require("../assets/Task_screen/cal.png")} style={styles.taskbar_icon}/>
         </View>
         <View style={styles.searchcontainer}>
           <View style={styles.searchbar}>
+            <View style={styles.searchinputcontainer}>
+              <TextInput
+                style={styles.input}
+                onChangeText={this.set_search_text}
+                value={this.search_text}
+              />
+            </View>
             <Image source={require("../assets/Task_screen/search.png")} style={styles.search_icon}/>
           </View>
           <View style={styles.sorticoncontainer}>
@@ -26,6 +48,7 @@ export default class Task_screen extends React.Component{
         <View style={styles.taskcontainer}>
         </View>
       </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
@@ -48,19 +71,34 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     backgroundColor: '#F0F0F0',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   searchbar: {
-    flex: 6,
+    flex: 5,
+    flexDirection: 'row',
     backgroundColor: '#FFFFFF',
-    justifyContent: 'center',
-    margin: 12,
+    justifyContent: 'flex-end',
+    alignItems:'center',
+    margin: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
     borderRadius: 30,
   },
   search_icon: {
-    flex:1,
     aspectRatio : 1,
-    backgroundColor:"#000000",
+    backgroundColor:"#FFFFFF",
+  },
+  searchinputcontainer: {
+    flex:7,
+  },
+  input: {
+    height: "100%",
+    width: "100%",
+    margin: 0,
+    borderWidth: 0,
+    paddingLeft:20,
+    // backgroundColor:"red",
+    fontSize: 25
   },
   sorticoncontainer: {
     flex: 1,
@@ -74,7 +112,7 @@ const styles = StyleSheet.create({
     width: "10%",
     aspectRatio : 1,
     backgroundColor:"#F0F0F0",
-    margin:5
+    margin:"20%"
   },
   taskcontainer: {
     flex: 8,
@@ -83,7 +121,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   taskbar_icon: {
-    width: "10%",
+    width: "8%",
     aspectRatio : 1
   },
 })
